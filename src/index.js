37d9a3863234a5c28ce48dc39e5c7d4ffa69c98a
brookaroo10 <brookedev10@protonmail.com>
2022-11-25 19:01:56 -0500

    fixing issues updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+//this is probably where my problem begins ---BM---
+
+//see this video: https://www.youtube.com/watch?v=jLS0TkAHvRg
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
